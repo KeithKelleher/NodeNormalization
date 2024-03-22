c24228c73dfa744a189d9d117a3a324f94c05dc1
KeithKelleher <keith.kelleher@ncats.nih.gov>
2024-03-22 15:27:45 -0400

    create workflow to download compendia data create docker compose file to host redis and api

---
+++ workflows/expand_compendia_files.py
@@ -0,0 +1,54 @@
+import os
+import json
+import requests
+
+def yield_data_file(file_list, base_url):
+    for file in file_list:
+        file_url = f"{base_url}{file}"
+        response = requests.head(file_url)
+        if response.status_code == 200:
+            file_size = int(response.headers.get('content-length', 0))
+            yield {
+                'file': file,
+                'url': file_url,
+                'size': file_size
+            }
+        else:
+            index = 0
+            while True:
+                file_segment = f"{file}.{str(index).zfill(2)}"
+                file_url = f"{base_url}{file_segment}"
+                response = requests.head(file_url)
+                if response.status_code == 200:
+                    file_size = int(response.headers.get('content-length', 0))
+                    yield {
+                        'file': file_segment,
+                        'url': file_url,
+                        'size': file_size
+                    }
+                    index += 1
+                elif response.status_code == 404:
+                    if index == 0:
+                        print(f"Failed: {file}")
+                    break
+                else:
+                    print(f"Error: Unexpected status code {response.status_code}")
+                    break
+
+def get_conflation_info(config_file):
+    conflation_url = os.environ.get('COMPENDIUM_URL') + 'conflation/'
+    with open(config_file, 'r') as file:
+        config = json.load(file)
+        conflation_files = [conf['file'] for conf in config['conflations']]
+        return [
+            file_info for file_info in yield_data_file(conflation_files, conflation_url)
+        ]
+
+def get_compendium_info(config_file):
+    compendium_url = os.environ.get('COMPENDIUM_URL') + 'compendia/'
+    with open(config_file, 'r') as file:
+        config = json.load(file)
+        data_files = config['data_files']
+        return [
+            file_info for file_info in yield_data_file(data_files, compendium_url)
+        ]
