86b0fc3638b8566001466e8adf786bbc341e34dc
KeithKelleher <keith.kelleher@ncats.nih.gov>
2024-04-15 15:36:36 -0400

    download segmented files before downloading the complete file

--- workflows/expand_compendia_files.py
+++ workflows/expand_compendia_files.py
@@ -4,36 +4,41 @@ import requests
 
 def yield_data_file(file_list, base_url):
     for file in file_list:
-        file_url = f"{base_url}{file}"
-        response = requests.head(file_url)
-        if response.status_code == 200:
-            file_size = int(response.headers.get('content-length', 0))
-            yield {
-                'file': file,
-                'url': file_url,
-                'size': file_size
-            }
-        else:
-            index = 0
-            while True:
-                file_segment = f"{file}.{str(index).zfill(2)}"
-                file_url = f"{base_url}{file_segment}"
-                response = requests.head(file_url)
-                if response.status_code == 200:
-                    file_size = int(response.headers.get('content-length', 0))
-                    yield {
-                        'file': file_segment,
-                        'url': file_url,
-                        'size': file_size
-                    }
-                    index += 1
-                elif response.status_code == 404:
-                    if index == 0:
-                        print(f"Failed: {file}")
-                    break
-                else:
-                    print(f"Error: Unexpected status code {response.status_code}")
-                    break
+        index = 0
+        found_pieces = False
+
+        while True:
+            file_segment = f"{file}.{str(index).zfill(2)}"
+            file_url = f"{base_url}{file_segment}"
+            response = requests.head(file_url)
+            if response.status_code == 200:
+                found_pieces = True
+                file_size = int(response.headers.get('content-length', 0))
+                yield {
+                    'file': file_segment,
+                    'url': file_url,
+                    'size': file_size
+                }
+                index += 1
+            elif response.status_code == 404:
+                break
+            else:
+                print(f"Error: Unexpected status code {response.status_code}")
+                break
+
+        if not found_pieces:
+            file_url = f"{base_url}{file}"
+            response = requests.head(file_url)
+            if response.status_code == 200:
+                file_size = int(response.headers.get('content-length', 0))
+                yield {
+                    'file': file,
+                    'url': file_url,
+                    'size': file_size
+                }
+            else:
+                print(f"Found no file or segmented files for: {file}")
+
 
 def get_conflation_info(config_file):
     conflation_url = os.environ.get('COMPENDIUM_URL') + 'conflation/'
